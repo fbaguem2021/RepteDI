191515c96bd0c855fed1066cf5e0391d92ad146d
Francesc Bague Marti <fbaguem2021@cepnet.net>
2022-05-16 09:01:16 +0200

    Subida de proyecto

---
+++ app/src/main/java/com/example/reptedi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.reptedi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
